deaaaf44f16165c9bd2027a30d1fe18e4eef3103
Jose Carlos <arzajose@gmail.com>
2026-08-05 11:33:22 -0600

    Add real bookmark icon to save button with hover state

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -16,13 +16,25 @@ function NewsCard({ article }) {
     <li className="news-card">
   <div className="news-card__image-wrapper">
     <img className="news-card__image" src={urlToImage} alt={title} />
-    <button
-      className="news-card__save-button"
-      type="button"
-      title="Inicia sesión para guardar artículos"
-    >
-      Guardar
-    </button>
+   <button
+  className="news-card__save-button"
+  type="button"
+  title="Inicia sesión para guardar artículos"
+>
+  <svg
+    width="50"
+    height="50"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M26 12V27.9424L20.6182 23.7139L20 23.2285L19.3818 23.7139L14 27.9424V12H26Z"
+      stroke="currentColor"
+      strokeWidth="2"
+    />
+  </svg>
+</button>
   </div>
   <p className="news-card__date">{formatDate(publishedAt)}</p>
   <h3 className="news-card__title">{title}</h3>
